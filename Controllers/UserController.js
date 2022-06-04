88482e70409205609aba5bbb772faa90ac778696
AyuburRahman <aiburrahman17@gmail.com>
2022-06-04 20:10:15 +0600

    salting

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,4 +1,5 @@
 import UserModel from "../Models/userModel.js";
+import bcrypt from 'bcrypt';
 
 
 // get a User
@@ -31,6 +32,14 @@ export const updateUser = async(req, res) => {
      if(id === currentUserId || currentUserAdminStatus)
      {
          try {
+           
+            if(password)
+            {
+                const salt = await bcrypt.genSalt(10)
+                req.body.password = await bcrypt.hash(password, salt)
+            }
+
+
              const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true})
              res.status(200).json(user)
          } catch (error) {
