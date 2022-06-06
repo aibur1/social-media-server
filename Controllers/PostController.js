c40c1a48122bd5bf59baa9aff036cc84d493dedb
AyuburRahman <aiburrahman17@gmail.com>
2022-06-07 00:06:17 +0600

    completed api confiquration

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,4 +1,5 @@
 import PostModel from "../Models/postModel.js";
+import UserModel from "../Models/userModel.js";
 import mongoose from "mongoose";
 
 
@@ -66,4 +67,70 @@ export const deletePost = async (req, res)=>{
     } catch (error) {
         res.status(500).json(error)
     }
+}
+
+
+
+// like/dislike a post
+
+export const likePost = async (req, res)=>{
+    const id = req.params.id
+    const {userId} = req.body
+
+    try {
+        
+       const post = await PostModel.findById(id)
+       if(!post.likes.includes(userId))
+       {
+           await post.updateOne({ $push : {likes : userId}})
+           res.status(200).json("Post liked")
+       }
+       else{
+            await post.updateOne({ $pull : {likes : userId}})
+            res.status(200).json("Post Unliked")
+       }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+// Get Timeline Posts
+export const getTimelinePosts = async (req, res) => {
+      const userId = req.params.id
+
+      try {
+          
+        const currentUserPosts = await PostModel.find({userId : userId})
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match: {
+                    _id : new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup: {
+                    from : "posts",
+                    localField:"following",
+                    foreignField:"userId",
+                    as:"followingPosts"
+                }
+            },
+            {
+                $project:{
+                    followingPosts: 1,
+                    _id:0
+                }
+            }
+        ])
+        
+        res.status(200)
+        .json(currentUserPosts.concat(...followingPosts[0].followingPosts)
+        .sort((a, b)=>{
+            return b.createdAt - a.createdAt;   //means latest posts are comming fists
+        })
+        );
+        
+      } catch (error) {
+          res.status(500).json(error)
+      }
 }
