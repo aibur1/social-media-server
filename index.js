901d41f9c9d463d51ac1c68171beb12ff28ba42a
AyuburRahman <aiburrahman17@gmail.com>
2022-07-23 20:06:54 +0600

    solve cors error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from 'body-parser';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import AuthRoute from './Routes/AuthRoute.js';
 import UserRoute from './Routes/UserRoute.js';
 import PostRoute from './Routes/PostRoute.js';
@@ -12,6 +13,7 @@ const app = express();
 //Middleware
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_DB,
